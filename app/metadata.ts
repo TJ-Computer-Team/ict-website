e65c34a89407ffcbb6e1bf93eed7ac3cbd0eb9c9
Gabriel Xu <105399165+Gabriel-Xu@users.noreply.github.com>
2024-09-27 17:44:58 -0400

    Update metadata.ts

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,7 +1,7 @@
 export const metadata = {
   title: 'TJ Intermediate Computer Team',
-  description: 'The (un)official website of Thomas Jefferson High School for Science and Technology&apos;s Intermediate Computer Team',
+  description: 'The website of Thomas Jefferson High School for Science and Technology&apos;s Intermediate Computer Team',
   icons: {
-    icon: '/temp_logo.png',
+    icon: '/logo.png',
   },
 };
