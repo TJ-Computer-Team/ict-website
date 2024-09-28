7b38de05045cb6d1b79174d02a2337a999345247
ssbdragonfly <ssbdragonfly@gmail.com>
2024-09-27 21:43:31 -0400

    logo

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,6 +2,6 @@ export const metadata = {
   title: 'TJ Intermediate Computer Team',
   description: 'The website of Thomas Jefferson High School for Science and Technology&apos;s Intermediate Computer Team',
   icons: {
-    icon: '/logo.png',
+    icon: '/icon.png',
   },
 };
