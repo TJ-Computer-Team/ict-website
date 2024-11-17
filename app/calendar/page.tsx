63ea9e489576b8e8a736eec90c8eecbba4780664
ssbdragonfly <ssbdragonfly@gmail.com>
2024-11-17 14:48:58 -0500

    calendar change

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,18 +1,18 @@
 export default function CalendarPage() {
-    const calendarUrl = "https://calendar.google.com/calendar/embed?src=49ba045dfedf495b29eb0829da786020559763d5bdea2508edb369a08e79d178%40group.calendar.google.com&ctz=America%2FNew_York"
-  
-    return (
-      <div className="container mx-auto px-4 py-12">
-        <h1 className="text-4xl font-bold mb-8 text-center mt-12">ICT Calendar</h1>
-        <div className="bg-white p-4 rounded-lg shadow-md overflow-hidden">
-          <iframe
-            src={calendarUrl}
-            style={{ border: 0, width: '100%', height: '600px' }}
-            frameBorder="0"
-            scrolling="no"
-            title="ICT Google Calendar"
-          ></iframe>
-        </div>
+  const sheetUrl = "https://docs.google.com/spreadsheets/d/1H4DdJTlFXAZqA6W1yegPFmOJGctda-t4ZL-_mmLoxgY/edit?usp=sharing&rm=minimal#gid=0";
+
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-4xl font-bold mb-8 text-center mt-12">Computer Team Calendar</h1>
+      <div className="bg-white p-4 rounded-lg shadow-md overflow-hidden">
+        <iframe
+          src={sheetUrl}
+          style={{ border: 0, width: '100%', height: '600px' }}
+          frameBorder="0"
+          scrolling="yes"
+          title="Team Google Sheets Calendar"
+        ></iframe>
       </div>
-    )
-  }
+    </div>
+  );
+}
